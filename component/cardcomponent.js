52c7fd05cc1ee8fb7dfa8e6e156638deb7e458e9
inv-Karan <panditkaran0928@gmail.com>
2021-04-12 14:06:43 +0530

    packages installation.

--- component/cardcomponent.js
+++ component/cardcomponent.js
@@ -5,7 +5,7 @@ const CardComponent = (props) => {
   let { classes, onPageChange, label, imgSrc, sub, bookmark } = props
   return <Grid
     sm={2}
-    item className={[classes.card, classes.clickableCard]}
+    item className={[classes.card, classes.cardRectangle]}
     onClick={() => onPageChange()} >
     <div className={classes.labelStyle} >
       {label}
@@ -18,29 +18,51 @@ const CardComponent = (props) => {
 
 const STYLES = {
   card: {
-    width: "100%",
-    height: '139px',
-    borderRadius: '20px',
-    backgroundColor: '#ffffff',
+    // width: "100%",
+    // height: '139px',
+    // borderRadius: '8px',
+    // backgroundColor: '#ffffff',
+    // margin: 10,
+    // padding: 10,
+    // position: 'inherit'
+    position: 'inherit',
+    width: '334px',
+    height: '93px',
+    left: '21px',
+    top: '911px',
+    padding: 10,
     margin: 10,
-    padding: 10
+
+    background: '#FFFFFF',
+    borderRadius: '8px',
   },
   labelStyle: {
-    padding: '10px 15px',
-    fontFamily: 'Poppins',
-    fontWeight: 800,
-    fontSize: '16px',
+    position: 'inherit',
+    width: '78px',
+    height: '21px',
+    left: '37px',
+    top: '1035px',
+  
+    fontfamily: 'Public Sans',
+    fontstyle: 'normal',
+    fontweight: 600,
+    fontsize: '18px',
+    lineheight: '21px',
+
     color: '#227df2',
-    "&:hover": {
-      color: "rgba(0, 0, 0, 1)"
-    },
+
   },
-  clickableCard: {
-    cursor: 'pointer',
-    "&:hover": {
-      background: "rgba(255, 255, 255, 1)"
-    },
+  cardRectangle: {
+    width: "100%",
+    position: 'inherit',
+    color: '#227df2'
   }
+  // clickableCard: {
+  //   cursor: 'pointer',
+  //   "&:hover": {
+  //     background: "rgba(255, 255, 255, 1)"
+  //   },
+  // }
 }
 
 export default withStyles(STYLES)(CardComponent)
